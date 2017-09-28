363aa91c0e3eb263126ab865a3bd16a147172b85
DavidDeng <daviddenghaotian@163.com>
2017-09-28 16:19:38 -0400

    Changing to cmake version 3.5.1

---
+++ build/CMakeFiles/ET-Sat.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/CS/users/hdeng/.linux/Documents/ThinSAT/Main/main.c" "/home/CS/users/hdeng/.linux/Documents/ThinSAT/build/CMakeFiles/ET-Sat.dir/Main/main.c.o"
+  "/home/CS/users/hdeng/.linux/Documents/ThinSAT/mode_SCM/SCM.c" "/home/CS/users/hdeng/.linux/Documents/ThinSAT/build/CMakeFiles/ET-Sat.dir/mode_SCM/SCM.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
