d9a0e21972213c139c3f9d5b88c8522fb907d8a4
LeftShiftJones <jonesr1@mail.gvsu.edu>
2017-10-12 08:53:48 -0400

    Added loop and attempted resolution of conflict

---
+++ cmake-build-debug/CMakeFiles/ET-Sat.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ET-Sat.dir/Main/main.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_SCM/SCM.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_LVSM/LVSM.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_PLM/PLM.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_SMM/SMM.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_TMM/TMM.c.obj"
+  "CMakeFiles/ET-Sat.dir/Health_Safety_Check/health_safety_check.c.obj"
+  "ET-Sat.pdb"
+  "ET-Sat.exe"
+  "ET-Sat.exe.manifest"
+  "libET-Sat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ET-Sat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
