bd816752387512f1b983ea45601b068fbbe33f8d
LeftShiftJones <jonesr1@mail.gvsu.edu>
2017-10-17 15:37:32 -0400

    Ryan: test commit

---
+++ cmake-build-debug/CMakeFiles/ET-Sat.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/MainTests/mainTest.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/MainTests/mainTest.c.obj"
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/Sensors/Em_Sensor.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/Sensors/Em_Sensor.c.obj"
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/Sensors/Light_Sensor.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/Sensors/Light_Sensor.c.obj"
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/Sensors/Rotation_Sensor.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/Sensors/Rotation_Sensor.c.obj"
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/Sensors/Tether_Communication_Sensor'.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/Sensors/Tether_Communication_Sensor'.c.obj"
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/mode_SCM/SCM.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/mode_SCM/SCM.c.obj"
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/mode_SMM/SMM.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/mode_SMM/SMM.c.obj"
+  "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/mode_TMM/TMM.c" "C:/Users/slopp/CLionProjects/Satellite Project/ET-Sat/cmake-build-debug/CMakeFiles/ET-Sat.dir/mode_TMM/TMM.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ET-Sat.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ET-Sat.dir/MainTests/mainTest.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_SCM/SCM.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_SMM/SMM.c.obj"
+  "CMakeFiles/ET-Sat.dir/mode_TMM/TMM.c.obj"
+  "CMakeFiles/ET-Sat.dir/Sensors/Light_Sensor.c.obj"
+  "CMakeFiles/ET-Sat.dir/Sensors/Rotation_Sensor.c.obj"
+  "CMakeFiles/ET-Sat.dir/Sensors/Em_Sensor.c.obj"
+  "CMakeFiles/ET-Sat.dir/Sensors/Tether_Communication_Sensor'.c.obj"
+  "ET-Sat.pdb"
+  "ET-Sat.exe"
+  "ET-Sat.exe.manifest"
+  "libET-Sat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ET-Sat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
